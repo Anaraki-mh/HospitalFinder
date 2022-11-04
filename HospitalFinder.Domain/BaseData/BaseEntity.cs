f5e8f0d9f8e87f9a164daf388a939e948b7f30b3
AnarakiMh <m.anaraki.1382@gmail.com>
2022-11-04 22:58:32 +0330

    Added BaseEntity to Domain

---
+++ HospitalFinder.Domain/BaseData/BaseEntity.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalFinder.Domain.BaseData
+{
+    public class BaseEntity
+    {
+        #region Constructor
+
+        public BaseEntity()
+        {
+            CreateDateTime = DateTime.UtcNow;
+            UpdateDateTime = DateTime.UtcNow;
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+        public DateTime UpdateDateTime { get; set; }
+
+        #endregion
+    }
+}
