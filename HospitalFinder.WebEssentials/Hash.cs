d1371fc73605296293954dfe460a57da87a3ed01
Anaraki-mh <m.anaraki.1382@gmail.com>
2023-03-17 02:45:53 +0330

    Added two new helper classes to create hashes and url-friendly strings

---
+++ HospitalFinder.WebEssentials/Hash.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalFinder.WebEssentials
+{
+    public static class Hash
+    {
+        public static string SHA256(string inputString)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+    }
+}
---
+++ HospitalFinder.WebEssentials/Url.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalFinder.WebEssentials
+{
+    public class Url
+    {
+        private static List<char> UnwiseOrReservedChars = new List<char>
+        {
+            ' ' ,'{' , '}' , '|' , '\\' , '^' , '[' , ']' , '`', '<', '>', ';' , '/' , '?' , ':' , '@' , '&' , '=' , '+' , '$' , ',', '.'
+        };
+
+        public static string CreateUrlFriendlyString(string title)
+        {
+            foreach (var character in UnwiseOrReservedChars)
+            {
+                title = title.Replace(character, '-');
+            }
+            return title;
+        }
+    }
+}
